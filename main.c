2edb262e3af2980bc1c0f59acfb4c91cba4a10da
nicksettoon <nwsettoon+github@gmail.com>
2020-02-27 22:56:27 -0600

    little work done

--- main.c
+++ main.c
@@ -2,36 +2,46 @@
 #include <pthread.h>
 
 struct verify_set {
-    int num1;
-    int num2;
-    int num3;
+    int numbs[9];
+    int valid;
 };
 
-void* verifySudokuWorker(void* set_in)
+void* verifySudokuRow(void* set_in)
 {//worker thread of execution which verifies a line or a square of nine characters as a valid sudoku combo or not
     //cast incoming struct from void* to verify_set
     struct verify_set* workingset = (struct verify_set*) set_in;
 
-    printf("num1: %i", workingset->num1);
-    printf("num2: %i", workingset->num2);
-    printf("num3: %i", workingset->num3);
+}
+
+void* verifySudokuCol(void* set_in)
+{//worker thread of execution which verifies a line or a square of nine characters as a valid sudoku combo or not
+    //cast incoming struct from void* to verify_set
+    struct verify_set* workingset = (struct verify_set*) set_in;
+
+}
+
+void* verifySudokuBox(void* set_in)
+{//worker thread of execution which verifies a line or a square of nine characters as a valid sudoku combo or not
+    //cast incoming struct from void* to verify_set
+    struct verify_set* workingset = (struct verify_set*) set_in;
+
 }
 
 int main()
 {//home's where the main is.
 
     struct verify_set testset;
-    testset.num1 = 1;
-    testset.num2 = 5;
-    testset.num3 = 9;
     //import sudoku puzzle from file into 2d array
     //spawn threads
-    pthread_t tid;
+    pthread_t tids[6];
+    pthread_attr_t attrs[6];
 
-    pthread_attr_t attr;
-    pthread_attr_init(&attr); //init thread attributes
+    //generate test sets
 
-    pthread_create(&tid, &attr, verifySudokuWorker, &testset);
+    for (int i=0;i<6;i++){
+        pthread_attr_init(&attrs[i]); //init thread attributes
+        pthread_create(&tids[i], &attrs[i], verifySudokuWorker, &testset);
+    }
 
     //pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine) (void *), void *arg);
 
